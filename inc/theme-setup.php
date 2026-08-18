ade71cd5c342552f0f1abd7f3c02578873cbedb1
اسمك على GitHub <@>
2026-08-18 03:22:00 +0300

    mprove theme setup support

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -11,9 +11,25 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function trade_sphare_theme_setup() {
 
+    load_theme_textdomain(
+        'trade-sphare',
+        TRADE_SPHARE_PATH . '/languages'
+    );
+
     add_theme_support( 'title-tag' );
+
     add_theme_support( 'post-thumbnails' );
+
     add_theme_support( 'custom-logo' );
+
+    add_theme_support( 'editor-styles' );
+
+    add_editor_style( 'style.css' );
+
+    add_theme_support( 'align-wide' );
+
+    add_theme_support( 'responsive-embeds' );
+
 
     add_theme_support(
         'html5',
@@ -28,16 +44,25 @@ function trade_sphare_theme_setup() {
         )
     );
 
+
+    add_image_size(
+        'trade-sphare-card',
+        400,
+        250,
+        true
+    );
+
+
     register_nav_menus(
         array(
             'primary' => __( 'القائمة الرئيسية', 'trade-sphare' ),
             'footer'  => __( 'قائمة التذييل', 'trade-sphare' ),
         )
     );
+
 }
 
 add_action(
     'after_setup_theme',
     'trade_sphare_theme_setup'
 );
-
